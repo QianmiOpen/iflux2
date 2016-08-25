3474cbd01fd9a0ef65cec988f6e003036dd1de69
hufeng <hufeng@qianmi.com>
2016-08-26 00:00:04 +0800

    rebuild

--- dist/validator.js
+++ dist/validator.js
@@ -87,9 +87,13 @@ var Validator = function () {
               var ruleValue = ruleMap[rule];
               //封装传递给校验函数的参数
               var args = [];
+              //是不是boolean类型且为false的规则
+              var isRuleValueFalse = typeof ruleValue === 'boolean' && ruleValue === false;
+              //当前的字段不是必填项,切值为空
+              var isNotRequired = !ruleMap['required'] && !value;
 
               //如果规则的值为布尔类型且为false,跳过校验
-              if (typeof ruleValue === 'boolean' && ruleValue === false) {
+              if (isRuleValueFalse || isNotRequired) {
                 continue;
               } else if (typeof ruleValue === 'boolean') {
                 args.push(value);
