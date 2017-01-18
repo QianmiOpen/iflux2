f7c8118da2ef5f172161eb3233df0827130e6480
hufeng <hufeng@qianmi.com>
2017-01-18 23:17:00 +0800

    remove unusual module,

--- src/index.js
+++ src/index.js
@@ -4,23 +4,17 @@ import StoreProvider from './store-provider';
 import Relax from './relax';
 import Store from './store';
 import {Action, CtxStoreName} from './decorator';
-import Atom from './atom';
 import {QL} from './ql';
 import {DQL} from './dql';
-import Validator from './validator';
-import Util from './util';
 
 export {
   QL,
   DQL,
   msg,
-  Atom,
   Action,
   CtxStoreName,
   Actor,
-  Util,
   Relax,
   Store,
-  StoreProvider,
-  Validator
+  StoreProvider
 };
